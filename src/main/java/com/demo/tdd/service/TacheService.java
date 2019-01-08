e35f72c2f1171e97108c018456c5a78d02ef4f06
tafoca <konohamarufotso@gmail.com>
2019-01-08 11:38:45 +0100

    finish the integration test

--- src/main/java/com/demo/tdd/service/TacheService.java
+++ src/main/java/com/demo/tdd/service/TacheService.java
@@ -2,8 +2,17 @@ package com.demo.tdd.service;
 
 import com.demo.tdd.domain.Tache;
 import com.demo.tdd.domain.TacheRepository;
+import com.demo.tdd.domain.TacheResult;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+
+import org.slf4j.Logger;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
 
 @Service
 public class TacheService {
@@ -14,6 +23,7 @@ public class TacheService {
         this.TacheRepository = TacheRepository;
     }
 
+
     @Cacheable("Taches")
     public Tache getTacheDetails(String name) {
         Tache Tache = TacheRepository.findByName(name);
@@ -21,5 +31,18 @@ public class TacheService {
             throw new TacheNotFoundException();
         }
         return Tache;
+    }
+
+    Logger logger = LoggerFactory.getLogger(TacheService.class);
+
+    private static final String MANGA_SEARCH_URL="http://api.jikan.moe/search/manga/";
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    public  List<Tache> getTacheByTitle(String title) {
+
+        return restTemplate.getForEntity(MANGA_SEARCH_URL+title, TacheResult.class).getBody().getTaches();
+
     }
 }
--- src/main/java/com/demo/tdd/web/TacheController.java
+++ src/main/java/com/demo/tdd/web/TacheController.java
@@ -3,12 +3,17 @@ package com.demo.tdd.web;
 import com.demo.tdd.domain.Tache;
 import com.demo.tdd.service.TacheNotFoundException;
 import com.demo.tdd.service.TacheService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 public class TacheController {
-
+    @Autowired
     private TacheService service;
 
     public TacheController(TacheService service) {
@@ -23,5 +28,20 @@ public class TacheController {
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
     private void TacheNotFoundHandler(TacheNotFoundException ex) {}
+
+    @RequestMapping(value = "/async/{title}", method = RequestMethod.GET)
+    @Async
+    public CompletableFuture<List<Tache>> searchASync(@PathVariable(name = "title") String title) {
+
+        return CompletableFuture.completedFuture(service.getTacheByTitle(title));
+
+    }
+
+    @RequestMapping(value = "/sync/{title}", method = RequestMethod.GET)
+    public @ResponseBody CompletableFuture<List<Tache>> searchSync(@PathVariable(name = "title") String title) {
+
+        return (CompletableFuture<List<Tache>>) service.getTacheByTitle(title);
+
+    }
 
 }
